318318db9f589e08f80b84c9fb2ae6ec801ddddc
Guiran <150613697+GuiG2023@users.noreply.github.com>
2025-10-06 10:49:29 -0700

    ready to upload

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -1,4 +1,4 @@
-const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
+const char *dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
 
 /*
  * This routine performs a dgemv operation
@@ -6,7 +6,17 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
  * On exit, A and X maintain their input values.
  */
-void my_dgemv(int n, double* A, double* x, double* y) {
+void my_dgemv(int n, double *A, double *x, double *y)
+{
    // insert your code here: implementation of vectorized vector-matrix multiply
 
+   for (int i = 0; i < n; i++)
+   {
+      double sum = 0.0; // accumulate the dot product
+      for (int j = 0; j < n; j++)
+      {
+         sum += A[i * n + j] * x[j];
+      }
+      y[i] += sum; // update y with the dot product
+   }
 }
